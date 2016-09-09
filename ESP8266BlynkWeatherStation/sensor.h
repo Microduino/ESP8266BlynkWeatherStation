fae6f0a2293acdd996d3f51662db168d4f048ed7
radiumray <radiumray9@gmail.com>
2016-09-09 17:48:22 +0800

    update pm25

--- ESP8266BlynkWeatherStation/sensor.h
+++ ESP8266BlynkWeatherStation/sensor.h
@@ -15,28 +15,27 @@ float sensorPM25;
 
 
 float PM25() {
-  int data_s = 0;    //串口接收数据
-  int num = -1;      //串口接收数据计数
-  int sum = 0;       //校验和
-  int cal[5];        //接收数据缓存
+  uint8_t rxData = 0;    //串口接收数据
+  int8_t index = -1;      //串口接收数据计数
+  uint8_t checksum = 0;       //校验和
+  uint8_t buf[5];        //接收数据缓存
   float dustDensity = 0;  //PM2.5浓度
+  uint32_t pmTimer = millis() + 300;
   
   pmSerial.begin(2400);   //首先启动软串口
-  pmSerial.flush();       //清空串口缓存
   
-  while(1) { 
+  while(millis() < pmTimer) { 
     if(pmSerial.available() > 0) { //串口缓存有数据
-      data_s = pmSerial.read();   //读取串口缓存数据
-      if(data_s == 0xAA) {         //得到数据帧起始位
-        num = 0;                  //开始计数
+      rxData = pmSerial.read();   //读取串口缓存数据
+      if(rxData == 0xAA) {         //得到数据帧起始位
+        index = 0;                  //开始计数
       }
-      else if(num >= 0) {    
-        num++;                //读到数据，计数+1
-        cal[num-1] = data_s;  //数据保存到缓存中
-        if(num == 6) {         //读到数据帧最后一位
-          sum = cal[0] + cal[1] + cal[2] + cal[3];  //计算校验和
-          if(sum == cal[4] && cal[5] == 0xFF) {     //校验和匹配，数据帧最后一位为0xFF，说明接收的数据帧正常
-            dustDensity = (cal[0]*256 + cal[1])*(5.0/1024)*550;   //计算PM2.5浓度，单位ug/m3
+      else if(index >= 0) {    
+        buf[index++] = rxData;  //数据保存到缓存中
+        if(index == 6) {         //读到数据帧最后一位
+          checksum = buf[0] + buf[1] + buf[2] + buf[3];  //计算校验和
+          if(checksum == buf[4] && buf[5] == 0xFF) {     //校验和匹配，数据帧最后一位为0xFF，说明接收的数据帧正常
+            dustDensity = (buf[0]*256 + buf[1])*(5.0/1024)*550;   //计算PM2.5浓度，单位ug/m3
           }
           else {     //接收的数据不正常
             dustDensity = 0;    //浓度清零
@@ -44,6 +43,7 @@ float PM25() {
           break;
         }
       }
+      pmTimer = millis() + 300;
     }   
   }
   pmSerial.end();     //关闭软串口
